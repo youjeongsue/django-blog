801c60d1a3e5b1cd56552c06ebe79a81743dd1e0
youjeongsue <43227174+youjeongsue@users.noreply.github.com>
2020-04-28 20:09:29 +0900

    init

---
+++ blog-venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\youjeongsue\desktop\workspace\django\django-blog\blog-venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
